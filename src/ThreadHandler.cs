2630fdd8638207522b0304f5da1e08cbb6b1b2c3
TechCrafterGaming <markus-domaschke@gmx.de>
2023-05-07 21:17:17 +0200

    try to get running

---
+++ src/ThreadHandler.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace KIC
+{
+    public class ThreadHandler
+    {
+        //private Thread _thread;
+        // HACK
+        // BUG
+        // FIXME asdf
+        // XXX asdasd
+        // [] asdsads
+        // [ ] asdssadf
+        // [x] asdsaddasd
+        public StartThread(task){
+            return new Thread(task);
+        }
+    }
+}
+
+// public class ThreadHandler
+// {
+//     private Thread _thread;
+//     public ThreadHandler()
+//     {
+//         return new Thread(new ThreadStart(ThreadMethod));
+//     }
+//     public void Start()
+//     {
+//         _thread.Start();
+//     }
+//     public void Stop()
+//     {
+//         _thread.Abort();
+//     }
+// }
