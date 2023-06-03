2eb493b918dfeb9a5a02a116749c585f21a5f3a2
TechCrafterGaming <markus-domaschke@gmx.de>
2023-06-03 18:23:32 +0200

    added LanguageService (not tested)

---
+++ app/src/app/models/supportedLanguages.model.ts
@@ -0,0 +1,50 @@
+export interface ILanguages extends Array<string> {}
+
+export const defaultLanguage:ILanguages[0] = "en"
+
+export const supportedLanguages:ILanguages = [
+    'en',       // Englisch
+    'de',       // Deutsch
+    // 'fr',       // Französisch
+    // 'es',       // Spanisch
+    // 'it',       // Italienisch
+    // 'pt',       // Portugiesisch
+    // 'ja',       // Japanisch
+    // 'ko',       // Koreanisch
+    // 'zh-CN',    // Chinesisch (vereinfacht)
+    // 'zh-TW',    // Chinesisch (traditionell)
+    // 'ru',       // Russisch
+    // 'ar',       // Arabisch
+    // 'hi',       // Hindi
+    // 'bn',       // Bengalisch
+    // 'id',       // Indonesisch
+    // 'ms',       // Malaysisch
+    // 'th',       // Thailändisch
+    // 'vi',       // Vietnamesisch
+    // 'nl',       // Niederländisch
+    // 'sv',       // Schwedisch
+    // 'fi',       // Finnisch
+    // 'no',       // Norwegisch
+    // 'da',       // Dänisch
+    // 'pl',       // Polnisch
+    // 'cs',       // Tschechisch
+    // 'hu',       // Ungarisch
+    // 'tr',       // Türkisch
+    // 'el',       // Griechisch
+    // 'he',       // Hebräisch
+    // 'ro',       // Rumänisch
+    // 'sk',       // Slowakisch
+    // 'sl',       // Slowenisch
+    // 'hr',       // Kroatisch
+    // 'bg',       // Bulgarisch
+    // 'uk',       // Ukrainisch
+    // 'et',       // Estnisch
+    // 'lv',       // Lettisch
+    // 'lt',       // Litauisch
+    // 'sr',       // Serbisch
+    // 'sq',       // Albanisch
+    // 'mk',       // Mazedonisch
+    // 'hy',       // Armenisch
+    // 'ka',       // Georgisch
+    // 'is',       // Isländisch
+  ];
---
+++ app/src/app/services/language.service.ts
@@ -0,0 +1,99 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { Observable } from 'rxjs/internal/Observable';
+
+import { supportedLanguages, ILanguages, defaultLanguage } from '../models/supportedLanguages.model';
+
+@Injectable()
+export class LanguageService {
+  protected activeLanguageSubject : BehaviorSubject<string>;
+  public languageChanged = new EventEmitter<void>();
+
+  private translations       : { [key: string]: any } = {};
+  private language           : ILanguages[0] = defaultLanguage;
+  private supportedLanguages : ILanguages = supportedLanguages;
+
+  public get languageObservable() : Observable<string> {
+    return this.activeLanguageSubject.asObservable();
+  }
+
+  // * constructor **************************************************
+  constructor() {
+    // Initialisierung des BehaviorSubject für die aktive Sprache
+    this.activeLanguageSubject = new BehaviorSubject<string>(this.getLanguage());
+    // Laden der Übersetzungsdateien
+    this.loadTranslations();
+  }
+  
+  // * functions ****************************************************
+  private loadTranslations() {
+    //! Annahme: Die JSON-Dateien sind im geforderten Ordner und haben entsprechende Namen
+    supportedLanguages.forEach(lang => {
+      // Laden der Übersetzungsdatei
+      const translation = require(`../assets/lang/${lang}.json`);
+      this.translations[lang] = translation;
+    });
+  }
+
+  public changeLanguage(language: string) {
+    // Überprüfung, ob die Sprache unterstützt wird
+    if (this.supportedLanguages.includes(language)) {
+      this.language = language;
+      // Auslösen des languageChanged-Ereignisses
+      this.languageChanged.emit();
+    } else {
+      console.error('Unsupported language:', language);
+    }
+  }
+
+  public getTranslation(key: string): string {
+    const translation = this.translations[this.language];
+    if (translation && translation[key]) {
+      return translation[key];
+    }
+    return key;
+  }
+  // Alias für getTranslation
+  public get(key: string): string {
+    return this.getTranslation(key);
+  }
+
+  private getLanguage(): string {
+    // Auslesen der Spracheinstellung aus dem Local Storage
+    // Standardwert falls keine Spracheinstellung vorhanden ist
+    this.language = localStorage.getItem('language') || defaultLanguage;
+    // Speichern der Spracheinstellung im Local Storage (falls erforderlich)
+    localStorage.setItem('language', this.language);
+    return this.language;
+  }
+}
+
+// @Injectable()
+// export class languageService {
+
+//   protected activeLanguageSubject: BehaviorSubject<any>;
+//   public languageChanged = new EventEmitter<object>();
+  
+//   protected language = localStorage.getItem("language")
+  
+//   public get themeObservable(): Observable<ILanguages> {
+//     return this.activeLanguageSubject.asObservable();
+//   }
+
+//   constructor() {
+//     this.activeLanguageSubject = new BehaviorSubject<any>(this.getLanguage());
+//   }
+
+//   public changeLanguage(language:ILanguages){
+//     this.language = language
+//     this.languageChanged.emit()
+//   }
+  
+//   private getLanguage() {
+//     this.language = localStorage.getItem("language") ? localStorage.getItem("language") : 'en'
+//     localStorage.setItem("language",this.language? this.language : "en")
+//     return this.language;
+//   }
+// }
+
+
